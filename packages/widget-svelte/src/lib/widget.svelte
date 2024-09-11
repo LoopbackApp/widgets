2465a0fdb82a2979f0d8be73e287fdd977fd1011
@mootoday <154029656+mootoday@users.noreply.github.com>
2024-09-11 16:07:46 -0700

    Improve UI

--- packages/widget-svelte/src/lib/widget.svelte
+++ packages/widget-svelte/src/lib/widget.svelte
@@ -15,41 +15,9 @@
 </script>
 
 <div
-	style={`
-    --background: 0 0% 100%;
-    --foreground: 0 0% 3.9%;
-    
-    --muted: 0 0% 96.1%;
-    --muted-foreground: 0 0% 45.1%;
-    
-    --popover: 0 0% 100%;
-    --popover-foreground: 0 0% 3.9%;
-    
-    --card: 0 0% 100%;
-    --card-foreground: 0 0% 3.9%;
-    
-    --border: 0 0% 89.8%;
-    --input: 0 0% 89.8%;
-    
-    --primary: 0 0% 9%;
-    --primary-foreground: 0 0% 98%;
-    
-    --secondary: 0 0% 96.1%;
-    --secondary-foreground: 0 0% 9%;
-    
-    --accent: 0 0% 96.1%;
-    --accent-foreground: 0 0% 9%;
-    
-    --destructive: 0 72.2% 50.6%;
-    --destructive-foreground: 0 0% 98%;
-    
-    --ring: 0 0% 3.9%;
-    
-    --radius: 0.5rem;
-    `}
-	class="lb-m-auto lb-bg-background lb-text-foreground lb-border-border lb-font-medium loopback-root lb-max-w-md lb-rounded-2xl lb-space-y-6 lb-px-6 lb-py-8"
+	class="lb-m-auto lb-bg-background lb-text-foreground lb-font-medium loopback-root lb-max-w-md lb-rounded-tr-2xl lb-space-y-6 lb-px-6 lb-py-8"
 >
-	<p class="lb-text-center lb-font-bold lb-text-black">Leave feedback</p>
+	<p class="lb-text-center lb-font-bold">Leave feedback</p>
 	<form action="post">
 		<input type="hidden" name="projectId" value={projectId} />
 		{#if selectedEmotion}
