d0971a238f3c732e3b2563e1ce5466ebcf0f887d
@mootoday <154029656+mootoday@users.noreply.github.com>
2024-10-02 21:43:46 -0700

    Submit feedback, add status message

--- packages/widget-custom-element/src/lib/custom-element.svelte
+++ packages/widget-custom-element/src/lib/custom-element.svelte
@@ -1,65 +1,61 @@
 <svelte:options customElement="loopback-widget" />
 
 <script lang="ts">
-  import { FeedbackWidget } from "@loopbackapp/widget-svelte";
+	import { FeedbackWidget } from "@loopbackapp/widget-svelte";
 
-  export let projectId: string;
+	export let projectId: string;
 </script>
 
-<div
-  role="dialog"
-  class="wrapper"
->
-  <div class="widget-wrapper">
-  <div class="title">Feedback 💬</div>
-    <FeedbackWidget
-      {projectId}
-    />
-  </div>
+<div role="dialog" class="wrapper">
+	<div class="widget-wrapper">
+		<div class="title">Feedback 💬</div>
+		<FeedbackWidget {projectId} />
+	</div>
 </div>
 
 <style>
-  .wrapper {
-    position: fixed;
-    height: 0px;
-    bottom: 0;
-    left: 1rem;
-    transition:
-      height 0.3s ease,
-      width 0.3s ease;
-  }
-  
-  .wrapper:hover {
-      height: fit-content;
-  }
-  
-  .widget-wrapper {
-    border: black solid 1px;
-    border-top-right-radius: 1rem;
-    border-bottom: 0;
-  }
-  
-  .widget-wrapper :global(.widget) {
-      border-top-left-radius: 0;
-      border-bottom-left-radius: 0;
-      border-bottom-right-radius: 0;
-  }
+	.wrapper {
+		position: fixed;
+		height: 0px;
+		bottom: 0;
+		left: 1rem;
+		transition:
+			height 0.3s ease,
+			width 0.3s ease;
+		font-family: sans-serif;
+	}
 
-  .title {
-      position: absolute;
-      top: -32px;
-      left: 0;
-      background-color: var(--lb-background);
-      color: var(--lb-foreground);
-    text-align: center;
-    width: 135px;
-    padding: 5px 10px;
-    cursor: pointer;
-    border-top: black solid 1px;
-    border-right: black solid 1px;
-    border-left: black solid 1px;
-    border-top-right-radius: 1rem;
-    border-top-left-radius: 1rem;
-    border-bottom: 0;
-  }
+	.wrapper:hover {
+		height: fit-content;
+	}
+
+	.widget-wrapper {
+		border: black solid 1px;
+		border-top-right-radius: 1rem;
+		border-bottom: 0;
+	}
+
+	.widget-wrapper :global(.widget) {
+		border-top-left-radius: 0;
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+
+	.title {
+		position: absolute;
+		top: -32px;
+		left: 0;
+		background-color: var(--lb-background);
+		color: var(--lb-foreground);
+		text-align: center;
+		width: 135px;
+		padding: 5px 10px;
+		cursor: pointer;
+		border-top: black solid 1px;
+		border-right: black solid 1px;
+		border-left: black solid 1px;
+		border-top-right-radius: 1rem;
+		border-top-left-radius: 1rem;
+		border-bottom: 0;
+	}
 </style>
