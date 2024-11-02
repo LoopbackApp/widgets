ebfbfea59c209ff576fb976684cdb75a035c6e9d
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-11-02 21:14:51 +0100

    chore: more adjustments

---
+++ packages/widget-react/src/lib/index.ts
@@ -0,0 +1,11 @@
+// Entry Point for library
+
+type User = {
+	id: string;
+	name: string;
+};
+
+export const user: User = {
+	id: "1",
+	name: "John Doe",
+};
--- packages/widget-react/vite.config.ts
+++ packages/widget-react/vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	build: {
+		lib: {
+			formats: ["es"],
+			entry: resolve(__dirname, "./src/lib/index.ts"),
+			fileName: (format) => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "tailwindcss"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+		sourcemap: true,
+		emptyOutDir: true,
+	},
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
+	plugins: [
+		dts({
+			entryRoot: resolve(__dirname, "./src/lib"),
+			tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
+		}),
+		react(),
+	],
+	server: {
+		proxy: {
+			"/api": "http://localhost:5173",
+		},
+	},
+});
