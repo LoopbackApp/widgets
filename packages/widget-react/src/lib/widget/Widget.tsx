ee408cf9343a48d4b159596bc32844a11419bb34
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2024-11-06 21:11:05 +0100

    feat: wire up logic

--- packages/widget-react/src/lib/widget/Widget.tsx
+++ packages/widget-react/src/lib/widget/Widget.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { EmojiForm } from "./form";
 import { Status } from "./status";
 import { useStyles } from "./useStyles";
@@ -11,14 +12,29 @@ export type LoopbackWidgetProps = {
 };
 export function Widget({ projectId }: LoopbackWidgetProps) {
 	const { isLoaded } = useStyles(projectId);
+	const [submitSuccess, setSubmitSuccess] = useState<boolean | null>(null);
+
+	useEffect(() => {
+		if (submitSuccess === null) return;
+		const timeout = setTimeout(() => {
+			setSubmitSuccess(null);
+		}, 5000);
+
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [submitSuccess]);
 
 	if (!isLoaded) return null;
 	return (
 		<>
 			<div className="lb-widget">
 				<div className={styles["widget"]}>
-					<Status type="success"></Status>
-					<EmojiForm />
+					{submitSuccess === false && <Status type="error"></Status>}
+					{submitSuccess === true && <Status type="success"></Status>}
+					{submitSuccess === null && (
+						<EmojiForm projectId={projectId} submitStateHandler={setSubmitSuccess} />
+					)}
 				</div>
 			</div>
 		</>
--- packages/widget-react/src/lib/widget/form.tsx
+++ packages/widget-react/src/lib/widget/form.tsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Emotion } from "./emotions";
 import styles from "./form.module.css";
+import { submitFeedback } from "./submit";
 
 const moods = Array.from({ length: 3 })
 	.fill(0)
 	.map((_, index) => index + 1);
 
-export function EmojiForm() {
+type Props = {
+	submitStateHandler: (submitSuccess: boolean | null) => void;
+	projectId: string;
+};
+export function EmojiForm({ submitStateHandler, projectId }: Props) {
 	const [selected, setSelected] = useState<number>(0);
+
+	const [note, setNote] = useState<string>("");
+	const [email, setEmail] = useState<string>("");
+
+	async function handleFormSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		try {
+			const res = await submitFeedback({
+				emotion: selected,
+				note,
+				type: "feedback",
+				email,
+				origin: window.location.href,
+				projectId,
+			});
+
+			submitStateHandler(res);
+		} catch (err) {
+			console.error("Failed to submit feedback:", err);
+			submitStateHandler(false);
+		}
+	}
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleFormSubmit} className={styles["form"]}>
 				<ul className={styles["list"]}>
 					{moods.map((mood) => (
 						<li key={mood}>
@@ -25,12 +52,23 @@ export function EmojiForm() {
 				</ul>
 				{!!selected && (
 					<div>
-						<input type="email" placeholder="Enter your email" />
-						<textarea placeholder="Add a comment"></textarea>
-						<button type="submit">Submit</button>
-						<div className="lb-powered-by">
-							powered by
-							<a href="https://loopback.works" target="_blank">
+						<input
+							onChange={(e) => setEmail(e.target.value)}
+							className={`${styles["input"]}`}
+							type="email"
+							placeholder="Enter your email"
+						/>
+						<textarea
+							onChange={(e) => setNote(e.target.value)}
+							className={`${styles["input"]} ${styles["textarea"]}`}
+							placeholder="Add a comment"
+						></textarea>
+						<button className={styles["button"]} type="submit">
+							Submit
+						</button>
+						<div className={styles["powered-by"]}>
+							powered by{" "}
+							<a style={{ color: "white" }} href="https://loopback.works" target="_blank">
 								Loopback
 							</a>
 						</div>
